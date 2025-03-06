1c9414c55175ae5277b18d43dbe932853feb79ff
Silantiev Jan <Silantiev-999@mail.ru>
2025-03-06 16:25:38 +0300

    1.1

---
+++ script.js
@@ -0,0 +1,40 @@
+let x = window.matchMedia("(max-width: 321px)");
+myFunction(x);
+function myFunction(x) {
+  if (x.matches) {
+    let swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  } else {
+    document.querySelector(".swiper-wrapper").classList.add("brands");
+    document
+      .querySelector(".swiper-wrapper")
+      .classList.remove("swiper-wrapper");
+
+    document.querySelector(".swiper").classList.remove("swiper", "mySwiper");
+
+    document.querySelectorAll(".swiper-slide").forEach((el) => {
+      el.classList.add("brands-high");
+      el.classList.remove("swiper-slide");
+    });
+  }
+}
+
+document.getElementById("hideButton").addEventListener("click", function () {
+  const child = window.matchMedia("(min-width: 769px)").matches ? 3 : 5;
+  const brandButtons = document.querySelectorAll(
+    `.brands-high:nth-last-child(-n+${child})`
+  );
+
+  brandButtons.forEach(function (button) {
+    button.style.display = button.style.display === "flex" ? "none" : "flex";
+  });
+  this.textContent = this.textContent === "Скрыть" ? "Показать все" : "Скрыть";
+  hideButton.classList.toggle("tap");
+
+  document
+    .querySelector(".hideButton::before")
+    .classList.add("hideButton--clicked::before");
+});
